b1a0d53c0d15c747baa04859ae8555cf7eb21e08
Peter <unfinishedideas@gmail.com>
2020-08-30 17:50:42 -0700

    adds react-router-dom instead to project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.scss';
 import { ScreenContext } from './context';
 // components
 import Header from './components/Header';
+// router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
@@ -12,11 +19,27 @@ function App() {
 
 
   return (
-    <ScreenContext.Provider value={{currentScreen, setCurrentScreen}}>
-      <div className="App">
-        <Header/>
-      </div>
-    </ScreenContext.Provider>
+    <Router>
+      <ScreenContext.Provider value={{currentScreen, setCurrentScreen}}>
+        <div className="App">
+          <Header/>
+        </div>
+        <Switch>
+          <Route path="/">
+            <p style={{color: 'white'}}>home</p>
+          </Route>
+          <Route path="/gallery">
+            <p style={{color: 'white'}}>gallery</p>
+          </Route>
+          <Route path="/music">
+            <p style={{color: 'white'}}>music</p>
+          </Route>
+          <Route path="/contact">
+            <p style={{color: 'white'}}>contact</p>
+          </Route>
+        </Switch>
+      </ScreenContext.Provider>
+    </Router>
   );
 }
 
